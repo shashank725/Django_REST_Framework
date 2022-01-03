eb967cd107ab90294982d86fad850817a9c5f903
shashank725 <shashank.nov725@gmail.com>
2022-01-03 14:15:07 +0530

    Serializer and Serializertion

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from app import views
+# from . import views
+
 app_name = 'app'
 urlpatterns = [
+    path('', views.apiOverview, name='apiOverview'),
+    path('student-list/', views.studentList, name='student-list'),
+    path('student-detail/<str:pk>/', views.studentDetail, name='student-detail')
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-from . import models
+from .serializers import StudentSerializer
+# from app import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
+
+from .models import Student
 
 # Create your views here.
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'List' : '/student-list',
+        'Detail View' : '/student-detail/<str:pk>/',
+        'Create' : '/student-create/',
+        'Update' : '/student-update/<str:pk>/',
+        'Delete' : '/student-delete/<str:pk>',
+    }
+    return Response(api_urls)
+
+@api_view(['GET'])
+def studentList(request):
+    student = Student.objects.all()
+    serializer = StudentSerializer(student, many=True)
+    # return Response(serializer.data)
+
+# To render data into JSON which is understandable by Frontend
+    # json_data = JSONRenderer().render(serializer.data)
+    # return HttpResponse(json_data, content_type='application/json')
+
+# To render data into JSON which is understandable by Frontend using JsonResponse
+    return JsonResponse(serializer.data, safe=False) #In order to allow non-dict objects to be serialized set the safe parameter to False (By default it is set to "True").
+
+
+@api_view(['GET'])
+def studentDetail(request, pk):
+    student = Student.objects.get(id=pk)
+    serializer = StudentSerializer(student, many=False)
+    return Response(serializer.data)
