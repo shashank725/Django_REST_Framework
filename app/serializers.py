3cd94ba6b69af4b4ef5f8cc35044b78b49501eab
shashank725 <shashank.nov725@gmail.com>
2022-01-07 01:14:33 +0530

    Model Serializer II

--- app/serializers.py
+++ app/serializers.py
@@ -46,7 +46,13 @@ from .models import Student
 
 # Model Serializer
 class StudentSerializer(serializers.ModelSerializer):
+# Validators
+    def starts_with_r(value):
+        if value[0].lower() != 'r':
+            raise serializers.ValidationError('Name should start with R')
+
     # name = serializers.CharField(read_only=True)
+    name = serializers.CharField(validators=[starts_with_r])
     class Meta:
         model = Student
         fields = ['name', 'roll', 'city']
