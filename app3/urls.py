eb8ce7374a1ceb7da27aa498ac75013475b67ab3
shashank725 <shashank.nov725@gmail.com>
2022-01-10 02:13:29 +0530

    Model View Set and Ready Only Model View Set

--- app3/urls.py
+++ app3/urls.py
@@ -6,8 +6,11 @@ from rest_framework.routers import DefaultRouter
 # Creating Router Object
 router = DefaultRouter()
 
-# Register StudentViewSet with Router
-router.register('studentapi', views.studentViewSet, basename='student')
+## Register StudentViewSet with Router
+# router.register('studentapi', views.studentViewSet, basename='student')
+# router.register('studentapi', views.studentModelViewSet, basename='student')
+router.register('studentapi', views.studentReadOnlyModelViewSet, basename='student')
+
 
 app_name = 'app3'
 urlpatterns = [
