f7f24995ba8e716ed88c0c8d6ae25af98ab94d45
shashank725 <shashank.nov725@gmail.com>
2022-01-19 03:19:13 +0530

    Authentication and Permission in Function Based View

--- app2/urls.py
+++ app2/urls.py
@@ -6,8 +6,8 @@ from app2 import views
 app_name = 'app2'
 urlpatterns = [
     # path('hello/', views.hello, name='hello'),
-    # path('studentapi/', views.studentApi, name='student-api'),   # Function based VIEW
-    # path('studentapi/<int:pk>/', views.studentApi, name='student-api'),    # Function based VIEW
+    path('studentapi/', views.studentApi, name='student-api'),   # Function based VIEW
+    path('studentapi/<int:pk>/', views.studentApi, name='student-api'),    # Function based VIEW
     # path('studentapi/', views.studentApi.as_view(), name='student-api'),    # Class based VIEW
     # path('studentapi/<int:pk>/', views.studentApi.as_view(), name='student-api'),    # Class based VIEW
     path('studentlist/', views.studentList.as_view(), name='student-list'),
